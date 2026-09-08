025815715bd283fe9f146290e656c312f0db85f8
getplus211-sys <getplus211@gmail.com>
2026-09-08 11:16:25 +0530

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import '../theme/app_theme.dart';
 import '../widgets/user_avatar.dart';
 import '../widgets/verified_badge.dart';
